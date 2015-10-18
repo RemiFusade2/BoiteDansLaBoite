41b25d818c620cbc8df653f306a5b83213d8f2de
Thomas De Mot <Thomasdemot@gmail.com>
2015-10-18 15:35:38 +0200

    [Prog] HUD + fix round script

--- Unity Project/Boite Dans La Boite/Assets/Scripts/RoundManager.cs
+++ Unity Project/Boite Dans La Boite/Assets/Scripts/RoundManager.cs
@@ -3,11 +3,16 @@ using System.Collections;
 
 public class RoundManager : MonoBehaviour {
 
+	public GUIStyle myGui;
+
 	public float initialTimer = 250.0f;
 	public float timer = 0.0f;
+	public float interRoundTimer = 3.0f;
 	public int currentRound = 0;
 
+	public bool interRound = false;
 	public bool upScore = false;
+	bool gameOver = false;
 
 	public int playerOneScore = 0;
 	public int playerTwoScore = 0;
@@ -21,24 +26,55 @@ public class RoundManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		timer -= Time.deltaTime;
+		if (!gameOver) {
+			if (!interRound)
+				timer -= Time.deltaTime;
+			
+			if (interRound) {
+				interRoundTimer -= Time.deltaTime;
+			}
+			
+			if (interRoundTimer <= 0.0f) {
+				interRoundTimer = 3.0f;
+				interRound = false;
+			}
+			
+			if (timer <= 0.0f) {
+				currentRound++;
+				timer = initialTimer;
+				interRound = true;
+			}
+			
+			if (currentRound == 0 && upScore == true || currentRound == 2 && upScore == true) {
+				playerOneScore += 10;
+				upScore = false;
+			}
+			
+			if (currentRound == 1 && upScore == true || currentRound == 3 && upScore == true) {
+				playerTwoScore += 10;
+				upScore = false;
+			}
 
-		if (timer <= 0.0f) 
+			if (currentRound == 4)
+				gameOver = true;
+		} 
+
+		else 
 		{
-			currentRound++;
-			timer = initialTimer;
+
 		}
 
-		if (currentRound == 0 && upScore == true || currentRound == 2 && upScore == true) 
-		{
-			playerOneScore += 10;
-			upScore = false;
-		}
+	}
 
-		if (currentRound == 1 && upScore == true || currentRound == 3 && upScore == true) 
-		{
-			playerTwoScore += 10;
-			upScore = false;
-		}
+	void OnGUI()
+	{
+
+		GUI.Label (new Rect (10, 10, 100, 20), "Player One Score : ", myGui); 
+		GUI.Label (new Rect (10, 35, 100, 20), playerOneScore.ToString(), myGui); 
+
+		GUI.Label (new Rect (Screen.width - 180, 10, 100, 20), "Player Two Score : ", myGui); 
+		GUI.Label (new Rect (Screen.width - 25, 35, 100, 20), playerTwoScore.ToString(), myGui); 
+
+
 	}
 }
