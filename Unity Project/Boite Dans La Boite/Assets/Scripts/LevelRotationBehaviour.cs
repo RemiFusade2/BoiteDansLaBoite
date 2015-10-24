473ccaa878ceed460ed461c47a66795e0633a26e
Thomas De Mot <Thomasdemot@gmail.com>
2015-10-24 20:00:34 +0200

    [Prog] Add 180° rotation when pressing 1 on num pad (first of three special powers for P2)

--- Unity Project/Boite Dans La Boite/Assets/Scripts/LevelRotationBehaviour.cs
+++ Unity Project/Boite Dans La Boite/Assets/Scripts/LevelRotationBehaviour.cs
@@ -9,11 +9,26 @@ public class LevelRotationBehaviour : MonoBehaviour
 	public float angleSpeed;
 
 	private float angle;
+	private float angle2;
+
+	public  float speed = 0.5f;
+
+	public float degree;
+
+	public bool powerActivated = false;
+
+	Quaternion target1;
+	Quaternion target2;
+	Quaternion target3;
+	Quaternion target4;
+
+	float oldZ;
 
 	// Use this for initialization
 	void Start () 
 	{
 		angle = 0;
+			
 	}
 	
 	// Update is called once per frame
@@ -35,5 +50,44 @@ public class LevelRotationBehaviour : MonoBehaviour
 			}
 		}
 		angle = 0;
+
+
+		//P2 special power - Appuie sur le "1" du num pad pour faire faire un 180° au niveau
+		if (Input.GetKeyDown ("[1]")) 
+		{
+			powerActivated = true;
+
+			Debug.Log(Levels[0].transform.rotation.z*100 + 180.0f);
+
+			target1 = Levels[0].transform.rotation * Quaternion.Euler (0, 0, 180.0f);
+			target2 = Levels[1].transform.rotation * Quaternion.Euler (0, 0, 180.0f);
+			target3 = Levels[2].transform.rotation * Quaternion.Euler (0, 0, 180.0f);
+			target4 = Levels[3].transform.rotation * Quaternion.Euler (0, 0, 180.0f);
+
+		}
+	
+		if (powerActivated == true) {
+			foreach (GameObject level in Levels) {
+				if (level.name == "Level")
+					level.transform.rotation = Quaternion.RotateTowards (level.transform.rotation, target1, speed);
+				else if (level.name == "Level - Middle rigth")
+					level.transform.rotation = Quaternion.RotateTowards (level.transform.rotation, target2, speed);
+				else if (level.name == "Level - Middle Left")
+					level.transform.rotation = Quaternion.RotateTowards (level.transform.rotation, target3, speed);
+				else if (level.name == "Level-Middle Back")
+					level.transform.rotation = Quaternion.RotateTowards (level.transform.rotation, target4, speed);
+
+				Debug.Log(target1.eulerAngles);
+			}
+
+			if(Levels[3].transform.rotation ==  target4)
+			{
+				powerActivated = false;
+				Debug.Log("I'm done :)");
+			}
+
+		}
+
+
 	}
 }
