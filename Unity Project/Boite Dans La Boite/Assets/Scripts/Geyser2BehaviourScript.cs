86577afedc606a36bad904d14b5c833fa9b48451
Remi <remi.fusade@gmail.com>
2015-10-25 18:52:04 +0100

    Ajout Geyser avec intégration des graphismes. (modification du script)
    Signed-off-by: Remi <remi.fusade@gmail.com>

---
+++ Unity Project/Boite Dans La Boite/Assets/Scripts/Geyser2BehaviourScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Geyser2BehaviourScript : MonoBehaviour {
+
+	public float warningTime;
+
+	private bool isAlreadyFiring;
+
+	// Use this for initialization
+	void Start () 
+	{
+		isAlreadyFiring = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{		
+		//P2 special power 2
+		if (Input.GetKeyDown ("[2]") && !isAlreadyFiring) 
+		{
+			isAlreadyFiring = true;
+			StartCountdown();
+		}	
+	}
+
+	public void StartCountdown()
+	{
+		this.GetComponent<Animator> ().SetTrigger ("Warning"); // l'animator est une machine à états, il gère tout seul le passage d'un état à un autre
+		StartCoroutine (WaitAndFire (warningTime)); // les coroutines permettent d'éviter les timers dans le code
+	}
+
+	IEnumerator WaitAndFire (float time)
+	{
+		yield return new WaitForSeconds (time);
+		Fire ();
+	}
+
+	public void Fire()
+	{
+		this.GetComponent<Animator> ().SetTrigger ("Fire");
+	}
+
+	// Cette méthode est appelée par l'animation "GeyserFire"
+	public void SetGeyserReady()
+	{
+		isAlreadyFiring = false;
+	}
+}
