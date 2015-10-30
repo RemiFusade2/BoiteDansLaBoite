4428cd795c98ef6a23336244b8bd1518b1db02b1
Remi <remi.fusade@gmail.com>
2015-10-30 17:34:58 +0100

    Ajout menu Start. Ralentissement de l'animation Idle du joueur.
    Signed-off-by: Remi <remi.fusade@gmail.com>

---
+++ Unity Project/Boite Dans La Boite/Assets/Scripts/MenuEngine.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuEngine : MonoBehaviour {
+
+	public GameObject mainMenuPanel;
+	public GameObject creditsPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowMainMenu()
+	{
+		creditsPanel.SetActive (false);
+		mainMenuPanel.SetActive (true);
+	}
+	
+	public void ShowCredits()
+	{
+		creditsPanel.SetActive (true);
+		mainMenuPanel.SetActive (false);
+	}
+
+	public void HideMainMenu()
+	{
+		mainMenuPanel.SetActive (false);
+	}
+}
